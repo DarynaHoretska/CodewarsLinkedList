b27d3a3c770fb18c3783804ab360360827480492
Daryna Horetska <daryna.gorecka@gmail.com>
2025-03-31 18:08:36 +0300

    problem move node photo and solution

---
+++ Linked Lists - Move Node/move_node.py.py
@@ -0,0 +1,15 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+class Context(object):
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+    
+def move_node(source, dest):
+    head = source.next
+    source.next = dest
+    dest = source
+    return Context(head, dest)
